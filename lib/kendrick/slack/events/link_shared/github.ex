e3d1f94cf77a4e0cf3f77edc6fb70fa15ec279ad
Kirill Kayumov <kirill.kayumov@flatstack.com>
2019-02-25 10:30:06 +0100

    Use emoji in github diff

--- lib/kendrick/slack/events/link_shared/github.ex
+++ lib/kendrick/slack/events/link_shared/github.ex
@@ -58,7 +58,7 @@ defmodule Kendrick.Slack.Events.LinkShared.Github do
           color: pull_request.color,
           text: """
           *#{pull_request.title}*
-          *Diff:* +#{pull_request.additions} / -#{pull_request.deletions}
+          *Diff:* :heavy_plus_sign:#{pull_request.additions} */* :heavy_minus_sign:#{pull_request.deletions}
           """
         }
 
