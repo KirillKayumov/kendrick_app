54dc6610f8de88ad93e9ec14cc1be6e77a5cb55e
Kirill Kayumov <kirill.kayumov@flatstack.com>
2019-05-17 12:23:11 +0200

    Sign out when auth token is invalid

--- lib/kendrick/auth/error_handler.ex
+++ lib/kendrick/auth/error_handler.ex
@@ -2,7 +2,10 @@ defmodule Kendrick.Auth.ErrorHandler do
   def auth_error(conn, {type, _reason}, _opts) do
     case type do
       :unauthenticated -> redirect_to_home(conn)
-      :invalid_token -> redirect_to_home(conn)
+      :invalid_token ->
+        conn
+        |> Kendrick.Guardian.Plug.sign_out()
+        |> redirect_to_home()
     end
   end
 
