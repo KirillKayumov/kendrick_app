86dfaba419506995896d7386144cd9a86cdb5e73
Kirill Kayumov <kirill.kayumov@flatstack.com>
2019-05-17 12:13:04 +0200

    Redirect to home page when token is invalid

--- lib/kendrick/auth/error_handler.ex
+++ lib/kendrick/auth/error_handler.ex
@@ -1,7 +1,12 @@
 defmodule Kendrick.Auth.ErrorHandler do
   def auth_error(conn, {type, _reason}, _opts) do
     case type do
-      :unauthenticated -> Phoenix.Controller.redirect(conn, to: "/")
+      :unauthenticated -> redirect_to_home(conn)
+      :invalid_token -> redirect_to_home(conn)
     end
+  end
+
+  defp redirect_to_home(conn) do
+    Phoenix.Controller.redirect(conn, to: "/")
   end
 end
